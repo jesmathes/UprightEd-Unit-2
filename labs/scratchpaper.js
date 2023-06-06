0a63694c06d0cf28ce6de8f13f5c2beea1d2b6a4
jessamathews@gmail.com <jessamathews@gmail.com>
2023-06-06 17:35:12 -0400

    fizzAndScratchpaper

---
+++ labs/scratchpaper.js
@@ -0,0 +1,92 @@
+// // let lineNumber;
+
+// // while (/* some test */) {
+// //   // your code here
+// // }
+
+let p = 100;
+
+for (let p = 0; p < 30; p++) {
+  console.log(p);
+
+}
+
+let lineNumber;
+
+while ('') {
+
+}
+
+// FOR IN LOOP
+let aka = "jEsS";
+let capName;
+
+for (let gl in aka) {
+  // console.log(typeof s);
+  if (gl == 0) {
+    capName = aka[gl].toLowerCase();
+    // console.log(capName);
+  } else {
+    capName += aka[gl].toUpperCase();
+  }
+}
+
+console.log(capName);
+
+// FOR OF
+
+let pies = ["rhubarb", "cherry", "apple", "pumpkin", "blueberry"];
+
+for (pie of pies) {
+  console.log(`I love ${pie} pie, more than I love you.`);
+}
+
+
+// WHILE
+
+
+
+let words = [
+  'potato,',
+  '4!',
+  'More!',
+];
+
+
+let rhyme = words[0];
+let count = 30;
+let pos = 0;
+
+while ((rhyme = !'More!')) {
+  console.log(`whatDisDo?`);
+}
+  
+while ((rhyme = ${count = 0; count < 30; p++})) {
+  console.log(${count});
+}
+
+
+
+
+let p = 100;
+
+for (let p = 0; p < 30; p++) {
+  console.log(p);
+}
+
+let lineNumber;
+
+while ("") { }
+
+```js
+/*
+- Write a function that takes two parameters:
+        - one parameter is for a first name,
+        - the other parameter is for a last name;
+        - have them come together in a variable inside the function.
+        - console.log 'Hello, my name is <your name>'
+        - call (or invoke) your function
+*/
+```
+
+let first = 
